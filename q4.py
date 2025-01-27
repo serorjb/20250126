ab3e3dbf26ed6aafb2c2211dcbcaab201f89aed1
serorjb <serorjb@gmail.com>
2025-01-27 10:58:01 +0000

    q3 bug fix

--- q4.py
+++ q4.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import seaborn as sns
+import warnings
 from matplotlib import pyplot as plt
-from pandas import Timestamp
-from pypfopt import risk_models, EfficientFrontier
 from pypfopt import plotting
+from pypfopt import risk_models, EfficientFrontier
+
+warnings.filterwarnings("ignore")
+from utils import backtest_portfolio, portfolio_metrics
+
+sns.set(style='whitegrid')
 # !pip install pyportfolioopt
 
 
@@ -11,8 +17,8 @@ prices = pd.read_pickle('hot/prices.pickle')
 scope = pd.read_pickle('hot/scope.pickle')
 
 # we will take monthly points starting from 2011-01-31 as that's where the eligible universe rebal dates start
-df = prices['2010-07-01':].dropna(axis=1, how='all').bfill()
-df = df.div(df.iloc[0])
+prices = prices['2010-07-01':].dropna(axis=1, how='all').bfill()
+ref = prices.div(prices.iloc[0])
 
 # however for the sake of having a proper matrix, we will use Ledoit-Wolf
 # and add 6 months of historical data points for lookback purposes
@@ -21,9 +27,8 @@ monthly_indices = extra_points.resample('M').last().index
 rebal_dates = scope.index.to_list()
 history = rebal_dates + list(monthly_indices)
 
-df = df[df.index.isin(history)]
+df = ref[ref.index.isin(history)]
 print(df)
-
 
 S = risk_models.CovarianceShrinkage(df).ledoit_wolf()
 plotting.plot_covariance(S, plot_correlation=True)
@@ -31,9 +36,66 @@ plt.savefig('plots/q4/ledoit_wolf_covariance_matrix.png')
 # plt.show()
 print(S)
 
+refresh_weights = False
+if refresh_weights:
+    results_dict = {}
+    for rebal_date in rebal_dates:
+        print(rebal_date)
+        df_subset = df[df.index <= rebal_date]
+        S = risk_models.CovarianceShrinkage(df_subset).ledoit_wolf()
+        ef = EfficientFrontier(None, S, weight_bounds=(None, None))
+        ef.min_volatility()
+        results_dict[rebal_date] = ef.clean_weights()
 
-S = risk_models.CovarianceShrinkage(df).ledoit_wolf()
-ef = EfficientFrontier(None, S, weight_bounds=(None, None))
-ef.min_volatility()
-weights = ef.clean_weights()
-print(weights)
+    results_df = pd.DataFrame.from_dict(results_dict, orient='index').sort_index()
+    resampled_results_df = results_df.reindex(ref.index).ffill()
+    pd.to_pickle(resampled_results_df, 'weights/opt_weights.pickle')
+    print(resampled_results_df)
+
+returns = pd.read_pickle('hot/returns.pickle')
+scope = pd.read_pickle('hot/scope.pickle')
+
+equity_curves: dict = dict()
+running_sharpe_ratios: dict = dict()
+
+weights: dict = dict(
+    equal_weights=pd.read_pickle('weights/equal_weights.pickle'),
+    opt_weights=pd.read_pickle('weights/opt_weights.pickle')
+)
+
+for approach_name, weights_frame in weights.items():
+    start = '2011-01-31'
+    backtest = backtest_portfolio(df_returns=returns[start:], df_weights=weights_frame[start:], rebal_cost=0)
+    equity_curves[approach_name] = backtest['equity']
+    running_sharpe_ratios[approach_name] = portfolio_metrics(backtest['equity'])
+    # adding 1 year to collect proper running sharpe ratio metrics
+    running_sharpe_ratios[approach_name] = running_sharpe_ratios[approach_name]['2012-01-31':]
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
+palette = sns.color_palette('muted', n_colors=len(equity_curves))
+for idx, (approach_name, equity_curve) in enumerate(equity_curves.items()):
+    sns.lineplot(data=equity_curve, label=approach_name, color=palette[idx], ax=ax1)
+
+ax1.set_title('Equity Curves Comparison', fontsize=16)
+ax1.set_xlabel('Time', fontsize=12)
+ax1.set_ylabel('Equity', fontsize=12)
+ax1.legend(title='Approach', fontsize=10)
+
+for idx, (approach_name, sharpe_ratio) in enumerate(running_sharpe_ratios.items()):
+    sns.lineplot(data=sharpe_ratio, label=approach_name, color=palette[idx], ax=ax2)
+
+ax2.set_title('Rolling Sharpe Ratio', fontsize=16)
+ax2.set_xlabel('Time', fontsize=12)
+ax2.set_ylabel('Sharpe Ratio', fontsize=12)
+ax2.legend(title='Approach', fontsize=10)
+
+plt.tight_layout()
+plt.savefig('plots/q4/equal-weights_vs_optimized_comparison.png')
+plt.show()
+
+
+# conclusion equal weights has a less returns but a higher Sharpe ratio than the naive portfolio,
+# which is somewhat expected ;)
+
+# thank you for reading me and thank you for proposing this exercise, I found it interesting
+# Jonathan
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 # RSI / MOMENTUM
@@ -14,3 +16,80 @@ def compute_rsi(series: pd.Series, period: int = 14):
 
 def compute_momentum(series: pd.Series, period: int = 21):
     return (series / series.shift(period) - 1).bfill()
+
+
+def backtest_portfolio(df_returns: pd.DataFrame, df_weights: pd.DataFrame, rebal_cost: float = float()):
+    # the below implementation does daily rebal with elements in scope for this month,
+    # one extension could be to use a threshold-based rebal frequency rather than time-based
+    symbols = df_returns.columns
+    df_backtest = pd.concat([df_returns.add_suffix('_RETURN'), df_weights.add_suffix('_WEIGHT')], axis=1)
+
+    for symbol in symbols:
+        df_backtest[symbol] = df_backtest[f'{symbol}_RETURN'] * df_backtest[f'{symbol}_WEIGHT'] * 100
+        if rebal_cost != 0.0:
+            df_backtest[symbol] = df_backtest[symbol].mask(
+                df_backtest[f'{symbol}_WEIGHT'] != df_backtest[f'{symbol}_WEIGHT'].shift(),
+                df_backtest[symbol] - rebal_cost)  # discounts rebal costs every time we change weights
+
+    df_backtest = df_backtest[symbols]
+    df_backtest['equity'] = (df_backtest + 1).mean(axis=1).cumprod(axis=0)
+
+    df_backtest['high_water_mark'] = df_backtest['equity'].cummax()
+    df_backtest['drawdown'] = (df_backtest['equity'] - df_backtest['high_water_mark']) / df_backtest[
+        'high_water_mark']
+
+    fig, axes = plt.subplots(2, 1, figsize=(12, 8), gridspec_kw={'height_ratios': [2, 1]})
+    # Plot equity curve and high-water mark
+    axes[0].plot(df_backtest['equity'], label='Equity Curve', linewidth=2)
+    axes[0].plot(df_backtest['high_water_mark'], label='High Water Mark', linestyle='--', color='red')
+    axes[0].set_title('Equity Curve and High Water Mark')
+    axes[0].set_xlabel('Time')
+    axes[0].set_ylabel('Equity')
+    axes[0].legend()
+    axes[0].grid(True)
+
+    # Plot drawdown
+    axes[1].plot(df_backtest['drawdown'], label='Running Drawdown', color='blue')
+    axes[1].fill_between(df_backtest.index, df_backtest['drawdown'], color='blue', alpha=0.3)
+    axes[1].set_title('Running Drawdown')
+    axes[1].set_xlabel('Time')
+    axes[1].set_ylabel('Drawdown')
+    axes[1].legend()
+    axes[1].grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+    return df_backtest[['equity', 'high_water_mark', 'drawdown']]
+
+
+def portfolio_metrics(equity_curve: pd.Series, risk_free_rate: float = float()) -> pd.Series:
+    # note we assume daily data with 252 business days per year to compute annualized metrics
+    daily_returns = equity_curve.pct_change().dropna()
+    high_water_mark = equity_curve.cummax()
+    drawdowns = (equity_curve - high_water_mark) / high_water_mark
+    max_drawdown = drawdowns.min() * 100
+    max_drawdown_duration = (drawdowns < 0).astype(int).groupby((drawdowns == 0).cumsum()).cumsum().max()
+
+    excess_returns = daily_returns - risk_free_rate / 252
+    sharpe_ratio = np.sqrt(252) * (excess_returns.mean() / excess_returns.std())
+    downside_returns = daily_returns[daily_returns < 0]
+    sortino_ratio = np.sqrt(252) * (excess_returns.mean() / downside_returns.std())
+    annualized_return = (equity_curve.iloc[-1] / equity_curve.iloc[0]) ** (252 / len(equity_curve)) - 1
+    calmar_ratio = annualized_return / abs(max_drawdown / 100)
+
+    running_sharpe = daily_returns.expanding().apply(lambda x: np.sqrt(252) * x.mean() / x.std())
+    daily_returns = equity_curve.pct_change().dropna()
+    volatility = daily_returns.std() * np.sqrt(252) * 100
+    years = (equity_curve.index[-1] - equity_curve.index[0]).days / 365.25
+    cagr = (equity_curve.iloc[-1] / equity_curve.iloc[0]) ** (1 / years) - 1
+
+    print(f'Max Drawdown: {max_drawdown:.2f}%')
+    print(f'Max Drawdown Duration: {max_drawdown_duration} days')
+    print(f'Sharpe Ratio: {sharpe_ratio:.2f}')
+    print(f'Sortino Ratio: {sortino_ratio:.2f}')
+    print(f'Calmar Ratio: {calmar_ratio:.2f}')
+    print(f'Volatility: {volatility:.2f}%')
+    print(f'CAGR: {cagr:.2%}')
+
+    return running_sharpe
